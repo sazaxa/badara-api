427ae9d8fe43e3f2d452f7d1612a8cce82a6a6b0
sazaxadev <weno@whosegoods.com>
2021-02-08 17:48:05 +0900

    cors를 위한 WebConfiguration 생성

--- src/test/java/com/sazaxa/shipmentapi/faq/FaqControllerTest.java
+++ src/test/java/com/sazaxa/shipmentapi/faq/FaqControllerTest.java
@@ -1,10 +1,19 @@
 package com.sazaxa.shipmentapi.faq;
 
+import com.sazaxa.shipmentapi.faq.dto.FaqRequestDto;
+import com.sazaxa.shipmentapi.faq.dto.FaqResponseDto;
+import com.sazaxa.shipmentapi.faq.dto.FaqSaveRequestDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(FaqController.class)
 class FaqControllerTest {
@@ -15,10 +24,39 @@ class FaqControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    String BASE_URL = "/api/v1/faq";
+
     @Test
-    void testGetAllFaq(){
+    void testGetAllFaq() throws Exception {
+        mockMvc.perform(get(BASE_URL))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testGetFaqById() throws Exception {
+        Long id = 1L;
+        mockMvc.perform(get(BASE_URL + "/" + id))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+     void testSaveFaq() throws Exception {
+
+        FaqResponseDto faqResponseDto = new FaqResponseDto(new Faq("t1", "c1"));
+
+        mockMvc.perform(post(BASE_URL))
+                .andExpect(status().isCreated());
+    }
+
+
+    void testUpdateFaq(){
 
     }
 
 
+    void testDeleteFaq(){
+
+    }
+
 }
+
