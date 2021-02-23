85946a3538092ae8a5fd6656be79db75692a9d3d
sazaxadev <weno@whosegoods.com>
2021-02-23 16:56:48 +0900

    비밀번호 암호화

---
+++ src/main/java/com/sazaxa/shipmentapi/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.sazaxa.shipmentapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    //비밀번호 암호화
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    
+}
