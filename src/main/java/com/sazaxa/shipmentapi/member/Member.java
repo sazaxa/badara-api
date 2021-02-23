4d8707e9c07624442cb0f0b8080c37a5ef30db24
sazaxadev <weno@whosegoods.com>
2021-02-23 18:19:40 +0900

    Role 추가

--- src/main/java/com/sazaxa/shipmentapi/member/Member.java
+++ src/main/java/com/sazaxa/shipmentapi/member/Member.java
@@ -1,11 +1,14 @@
 package com.sazaxa.shipmentapi.member;
 
+import com.sazaxa.shipmentapi.member.role.Role;
 import com.sazaxa.shipmentapi.util.BaseEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @NoArgsConstructor
 @Getter
@@ -31,13 +34,20 @@ public class Member extends BaseEntity {
     @Column
     private String status;
 
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "member_roles",
+            joinColumns = @JoinColumn(name = "member_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
     @Builder
-    public Member(String email, String password, String phoneNumber, String name, String status) {
+    public Member(String email, String password, String phoneNumber, String name, String status, Set<Role> roles) {
         this.email = email;
         this.password = password;
         this.phoneNumber = phoneNumber;
         this.name = name;
         this.status = status;
+        this.roles = roles;
     }
 
     public void updateMember(String email, String password, String phoneNumber, String name){
---
+++ src/main/java/com/sazaxa/shipmentapi/member/role/RoleName.java
@@ -0,0 +1,6 @@
+package com.sazaxa.shipmentapi.member.role;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
