6044905cc131dc961711fa64d567226c261a6fe1
sazaxadev <weno@whosegoods.com>
2021-02-23 17:52:56 +0900

    AuthController 작성 (주석처리)

--- src/main/java/com/sazaxa/shipmentapi/member/auth/AuthController.java
+++ src/main/java/com/sazaxa/shipmentapi/member/auth/AuthController.java
@@ -1,9 +1,37 @@
 package com.sazaxa.shipmentapi.member.auth;
 
+import com.sazaxa.shipmentapi.member.Member;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/auth")
 @RestController
 public class AuthController {
+
+    @PostMapping("/signup")
+    public ResponseEntity registerMember(@RequestBody Member resource ){
+
+//        if(memberService.isExistsByUserId(resource.getUserId())){
+//            return new ResponseEntity("userId is already taken", HttpStatus.BAD_REQUEST);
+//        }
+
+//        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+//                .orElseThrow(() -> new ResourceNotFoundException("User Role not set."));
+
+//        Member member = Member.builder()
+//                .userId(resource.getUserId())
+//                .password(passwordEncoder.encode(resource.getPassword()))
+//                .name(resource.getName())
+//                .roles(Collections.singleton(userRole))
+//                .build();
+//
+//        memberService.registerMember(member);
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
 }
