481b447044d1be6ba95dc080b67f9b25c61b6142
sazaxadev <weno@whosegoods.com>
2021-03-23 16:42:50 +0900

    feat: Product Entity 생성하라

---
+++ src/main/java/com/sazaxa/shipmentapi/product/Product.java
@@ -0,0 +1,46 @@
+package com.sazaxa.shipmentapi.product;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sazaxa.shipmentapi.order.Order;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Getter
+@NoArgsConstructor
+@Table
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String productDetail;
+
+    @Column
+    private Integer quantity;
+
+    @Column
+    private Double price;
+
+    @Column
+    private Double weight;
+
+    @ManyToOne(targetEntity = Order.class, fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"hibernateLazyInitializer"})
+    @JoinColumn(name="order_id")
+    private Order order;
+
+
+}
