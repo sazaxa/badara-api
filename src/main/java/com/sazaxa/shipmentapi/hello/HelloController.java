314540c9e15223e27c277f16ca3a4b282ad97795
sazaxadev <weno@whosegoods.com>
2021-04-07 12:03:10 +0900

    fix: HelloController 변경하라

---
+++ src/main/java/com/sazaxa/shipmentapi/hello/HelloController.java
@@ -0,0 +1,12 @@
+package com.sazaxa.shipmentapi.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping
+    public String successHello(){
+        return "helloSuccess";
+    }
+}
