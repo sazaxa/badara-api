2bab33817920ed5374a0c1e550267964e6f8e673
sazaxadev <weno@whosegoods.com>
2021-04-14 10:16:25 +0900

    fix: 필요한 변수들을 추가하라

--- src/main/java/com/sazaxa/shipmentapi/order/dto/OrderStatusRequestDto.java
+++ src/main/java/com/sazaxa/shipmentapi/order/dto/OrderStatusRequestDto.java
@@ -6,4 +6,10 @@ import lombok.Getter;
 public class OrderStatusRequestDto {
     private String paymentMethod;
     private String depositName;
+    private String cardType;
+    private String cardCompany;
+    private String cardNumber;
+    private String cardOwnerType;
+    private String paymentKey;
+    private String requestedAt;
 }
