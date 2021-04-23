ed80fd2dc081192330434a4ca7e0177a56f7b6a8
sazaxadev <weno@whosegoods.com>
2021-04-23 14:51:05 +0900

    faet: number로 찾을수 있는 로직을 추가하라

--- src/main/java/com/sazaxa/shipmentapi/excel/country/CountryRepository.java
+++ src/main/java/com/sazaxa/shipmentapi/excel/country/CountryRepository.java
@@ -2,5 +2,8 @@ package com.sazaxa.shipmentapi.excel.country;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CountryRepository extends JpaRepository<Country, Long> {
+    Optional<Country> findByNumber();
 }
