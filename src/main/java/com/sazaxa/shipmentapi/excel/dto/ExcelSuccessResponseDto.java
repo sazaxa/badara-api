54cda4884bfaec1e46578853f4d31896d3caba5f
sazaxadev <weno@whosegoods.com>
2021-04-23 10:42:43 +0900

    ExcelSuccessResponsDto 생성하라

--- src/main/java/com/sazaxa/shipmentapi/excel/dto/ExcelSuccessResponseDto.java
+++ src/main/java/com/sazaxa/shipmentapi/excel/dto/ExcelSuccessResponseDto.java
@@ -1,2 +1,8 @@
-package com.sazaxa.shipmentapi.excel.dto;public class ExcelSuccessResponseDto {
+package com.sazaxa.shipmentapi.excel.dto;
+
+public class ExcelSuccessResponseDto {
+    private String message;
+    public ExcelSuccessResponseDto() {
+        this.message = "upload success";
+    }
 }
