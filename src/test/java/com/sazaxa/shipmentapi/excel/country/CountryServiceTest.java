82e8f3192ce19b818aa0747a42e4a2a04f7b4d5e
sazaxadev <weno@whosegoods.com>
2021-04-23 11:43:59 +0900

    test: 엑셀파일 가져오는 테스트 성공하라

--- src/test/java/com/sazaxa/shipmentapi/excel/country/CountryServiceTest.java
+++ src/test/java/com/sazaxa/shipmentapi/excel/country/CountryServiceTest.java
@@ -2,7 +2,9 @@ package com.sazaxa.shipmentapi.excel.country;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
@@ -10,14 +12,21 @@ class CountryServiceTest {
 
     private CountryRepository countryRepository;
     private CountryService countryService;
+    private ClassPathResource file;
 
     @BeforeEach
     void setUp() {
         countryRepository = mock(CountryRepository.class);
         countryService = new CountryService(countryRepository);
+        file = new ClassPathResource("국가정보_21423.xlsx");
     }
 
     @Test
-    void create() {
+    void testGetFile(){
+        assertThat(file.getFilename()).isEqualTo("국가정보_21423.xlsx");
+    }
+    
+    @Test
+    void testCreate() {
     }
 }
