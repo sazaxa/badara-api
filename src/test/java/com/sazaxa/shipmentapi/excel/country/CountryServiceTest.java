83cb81a89c72dce4e58b3ea39eb3cb231a451966
sazaxadev <weno@whosegoods.com>
2021-04-23 14:14:55 +0900

    test: 엑셀 업로드한 값 테스트하라

--- src/test/java/com/sazaxa/shipmentapi/excel/country/CountryServiceTest.java
+++ src/test/java/com/sazaxa/shipmentapi/excel/country/CountryServiceTest.java
@@ -1,11 +1,18 @@
 package com.sazaxa.shipmentapi.excel.country;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 class CountryServiceTest {
@@ -18,15 +25,29 @@ class CountryServiceTest {
     void setUp() {
         countryRepository = mock(CountryRepository.class);
         countryService = new CountryService(countryRepository);
-        file = new ClassPathResource("국가정보_21423.xlsx");
+        file = new ClassPathResource("국가정보_210423.xlsx");
     }
 
     @Test
     void testGetFile(){
-        assertThat(file.getFilename()).isEqualTo("국가정보_21423.xlsx");
+        assertThat(file.getFilename()).isEqualTo("국가정보_210423.xlsx");
     }
-    
+
     @Test
-    void testCreate() {
+    void testCreate() throws IOException {
+        Workbook workbook = new XSSFWorkbook(file.getInputStream());
+        Sheet sheet = workbook.getSheetAt(0);
+        List<Country> countryList = new ArrayList<>();
+        for (Row row : sheet){
+            if (row.getRowNum() == 0){
+                continue;
+            }
+            countryList.add(Country.builder()
+                    .name(row.getCell(0).getStringCellValue())
+                    .number((int) row.getCell(1).getNumericCellValue())
+                    .code(row.getCell(2).getStringCellValue())
+                    .build());
+        }
+        assertThat(countryList.size()).isEqualTo(233);
     }
 }
