ccae044bfd959a8917429d428e9003a579ee54d3
sazaxadev <weno@whosegoods.com>
2021-05-11 12:23:42 +0900

    feat: 주문정보 엑셀다운로드 추가하라

--- src/main/java/com/sazaxa/shipmentapi/excel/download/ExcelDownloadController.java
+++ src/main/java/com/sazaxa/shipmentapi/excel/download/ExcelDownloadController.java
@@ -1,9 +1,13 @@
 package com.sazaxa.shipmentapi.excel.download;
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 @RestController
 public class ExcelDownloadController {
@@ -15,9 +19,11 @@ public class ExcelDownloadController {
     }
 
     @GetMapping("/excel/download/orders")
-    public ByteArrayInputStream downloadAllOrders(){
-        excelDownloadService.downloadAllOrders();
-        return null;
+    public ResponseEntity<InputStreamResource> downloadAllOrders() throws IOException {
+        ByteArrayInputStream inputStream = excelDownloadService.downloadAllOrders();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment; filename=order.xlsx");
+        return ResponseEntity.ok().headers(headers).body(new InputStreamResource(inputStream));
     }
 
 }
