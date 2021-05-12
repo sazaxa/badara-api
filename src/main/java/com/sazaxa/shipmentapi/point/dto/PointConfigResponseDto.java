86e1d62595786186731a6d134b5a59d951b78007
sazaxadev <weno@whosegoods.com>
2021-05-12 17:08:02 +0900

    feat: PointConfigResponseDto of 메소드 생성하라

--- src/main/java/com/sazaxa/shipmentapi/point/dto/PointConfigResponseDto.java
+++ src/main/java/com/sazaxa/shipmentapi/point/dto/PointConfigResponseDto.java
@@ -1,5 +1,6 @@
 package com.sazaxa.shipmentapi.point.dto;
 
+import com.sazaxa.shipmentapi.point.entity.Point;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,5 +17,13 @@ public class PointConfigResponseDto {
         this.id = id;
         this.isRegisterActive = isRegisterActive;
         RegisterAmount = registerAmount;
+    }
+
+    public static PointConfigResponseDto of(Point point) {
+        return PointConfigResponseDto.builder()
+                .id(point.getId())
+                .isRegisterActive(point.getIsRegisterActive())
+                .registerAmount(point.getRegisterAmount())
+                .build();
     }
 }
