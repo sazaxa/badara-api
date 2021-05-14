48204f4936f4a1e4b6f39e41db75c0c201648785
sazaxadev <weno@whosegoods.com>
2021-05-14 11:01:15 +0900

    fix: pointHistory Entity에 setion, member, order 컬럼 추가하라

--- src/main/java/com/sazaxa/shipmentapi/point/entity/PointHistory.java
+++ src/main/java/com/sazaxa/shipmentapi/point/entity/PointHistory.java
@@ -1,10 +1,21 @@
 package com.sazaxa.shipmentapi.point.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sazaxa.shipmentapi.member.Member;
+import com.sazaxa.shipmentapi.order.Order;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @NoArgsConstructor
 @Getter
@@ -26,14 +37,30 @@ public class PointHistory {
     private Double withdraw;
 
     @Column
+    private String section;
+
+    @Column
     private String detail;
 
+    @ManyToOne(targetEntity = Member.class, fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"hibernateLazyInitializer"})
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    @ManyToOne(targetEntity = Order.class, fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"hibernateLazyInitializer"})
+    @JoinColumn(name="order_id")
+    private Order order;
+
     @Builder
-    public PointHistory(Long id, Double balance, Double deposit, Double withdraw, String detail) {
+    public PointHistory(Long id, Double balance, Double deposit, Double withdraw, String section, String detail, Member member, Order order) {
         this.id = id;
         this.balance = balance;
         this.deposit = deposit;
         this.withdraw = withdraw;
+        this.section = section;
         this.detail = detail;
+        this.member = member;
+        this.order = order;
     }
 }
