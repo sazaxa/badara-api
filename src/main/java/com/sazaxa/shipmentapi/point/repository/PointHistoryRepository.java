1cf80b1d3fb85503bdf70cb784d3f2c0ebb9269f
sazaxadev <weno@whosegoods.com>
2021-05-17 14:58:15 +0900

    feat: order로 pointHistory 조회하는 로직 추가하라

--- src/main/java/com/sazaxa/shipmentapi/point/repository/PointHistoryRepository.java
+++ src/main/java/com/sazaxa/shipmentapi/point/repository/PointHistoryRepository.java
@@ -1,7 +1,11 @@
 package com.sazaxa.shipmentapi.point.repository;
 
+import com.sazaxa.shipmentapi.order.Order;
 import com.sazaxa.shipmentapi.point.entity.PointHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PointHistoryRepository extends JpaRepository<PointHistory, Long> {
+    Optional<PointHistory> findByOrder(Order order);
 }
