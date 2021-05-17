8841a42c3c7a494a7755c7f1fdfea5a7e9f3e162
sazaxadev <weno@whosegoods.com>
2021-05-17 12:37:35 +0900

    test: 상태값 비교하는 테스트 성공하라

---
+++ src/test/java/com/sazaxa/shipmentapi/order/OrderTest.java
@@ -0,0 +1,26 @@
+package com.sazaxa.shipmentapi.order;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OrderTest {
+
+    private Order order;
+
+
+    @BeforeEach
+    void setUp(){
+        order = Order.builder()
+                .orderStatus(OrderStatus.PAYMENT_BANK)
+                .build();
+    }
+
+    @Test
+    void 무통장입금인상태에서_결제요청이왔을때_OrderStatus와비교하는_테스트(){
+        assertThat(order.getOrderStatus().name()).isEqualTo(OrderStatus.PAYMENT_BANK.name());
+    }
+
+
+}
