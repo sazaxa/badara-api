7f246bbcff94948a59db1e1ad2ad5fd3116180db
sazaxadev <weno@whosegoods.com>
2021-05-24 13:24:26 +0900

    refactor: 불필요한 테스트코드 제거하라

--- src/test/java/com/sazaxa/shipmentapi/box/BoxServiceTest.java
+++ src/test/java/com/sazaxa/shipmentapi/box/BoxServiceTest.java
@@ -1,13 +1,7 @@
 package com.sazaxa.shipmentapi.box;
 
-import com.sazaxa.shipmentapi.box.dto.BoxUpdateRequestDto;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 class BoxServiceTest {
@@ -21,24 +15,5 @@ class BoxServiceTest {
         boxRepository = mock(BoxRepository.class);
         boxService = new BoxService(boxRepository);
     }
-
-//    @Test
-//    void updateCenterIncome() {
-//        Box box = Box.builder()
-//                .id(EXISTED_ID)
-//                .expectedPrice(20D)
-//                .build();
-//
-//
-//        BoxUpdateRequestDto boxUpdateRequest = BoxUpdateRequestDto.builder()
-//                .koreanInvoice("test-invoice")
-//                .koreanShippingCompany("test-company")
-//                .build();
-//
-//        given(boxRepository.findById(EXISTED_ID)).willReturn(Optional.of(box));
-//        Box newBox = boxService.updateCenterIncome(EXISTED_ID, boxUpdateRequest);
-//
-//        assertThat(newBox.getKoreanInvoice()).isEqualTo("test-invoice");
-//    }
 
 }
