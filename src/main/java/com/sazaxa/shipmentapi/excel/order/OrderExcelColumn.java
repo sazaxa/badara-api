83afd987952c236f1b033db8fb12decc3a455a70
sazaxadev <weno@whosegoods.com>
2021-06-22 09:48:53 +0900

    feat: 오더 엑셀 컬럼값을 위한 enum 생성하라

---
+++ src/main/java/com/sazaxa/shipmentapi/excel/order/OrderExcelColumn.java
@@ -0,0 +1,31 @@
+package com.sazaxa.shipmentapi.excel.order;
+
+import lombok.Getter;
+
+@Getter
+public enum OrderExcelColumn {
+    RECIPIENT_NAME,
+    EMAIL,
+    NATION,
+    ADDRESS,
+    ZIPCODE,
+    CITY,
+    STATE,
+    RECIPIENT_MEMO,
+    PHONE_NUMBER,
+    PACKAGE_TYPE,
+    WIDTH,
+    DEPTH,
+    HEIGHT,
+    NET_WEIGHT,
+    BOX_MEMO,
+    PRODUCT_NAME_1,
+    QTY_1,
+    PRICE_1,
+    PRODUCT_NAME_2,
+    QTY_2,
+    PRICE_2,
+    PRODUCT_NAME_3,
+    QTY_3,
+    PRICE_3
+}
